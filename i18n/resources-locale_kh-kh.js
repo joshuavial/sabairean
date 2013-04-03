18a188993347ca0325f20d035a5064baedf81573
Joshua Vial <joshua@enspiral.com>
2013-04-03 20:34:10 +1300

    localisation for home page

--- i18n/resources-locale_kh-kh.js
+++ i18n/resources-locale_kh-kh.js
@@ -8,5 +8,21 @@
     "key" : "__chapters__",
     "value" : "Chapters--",
     "description" : "Heading for chapters"
+  },
+  {
+    "key"   : "__next__",
+    "value" : "Next--"
+  },
+  {
+    "key"   : "__previous__",
+    "value" : "Previous--"
+  },
+  {
+    "key"   : "setup_android_environment",
+    "value" : "Setup your android environment--"
+  },
+  {
+    "key"   : "deploy_android_app",
+    "value" : "Deploy your android app--"
   }
 ]
