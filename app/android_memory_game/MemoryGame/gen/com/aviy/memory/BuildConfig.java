6dfe7915da2f062ddf43fab514e74f9e2840da17
reaksmey <reaksmey@instedd.org>
2013-04-05 16:15:37 +0700

    add android memory game to the project

---
+++ app/android_memory_game/MemoryGame/gen/com/aviy/memory/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.aviy.memory;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ app/android_memory_game/MemoryGame/src/com/aviy/memory/Card.java
@@ -0,0 +1,19 @@
+package com.aviy.memory;
+
+import android.widget.Button;
+
+
+public class Card{
+
+	public int x;
+	public int y;
+	public Button button;
+	
+	public Card(Button button, int x,int y) {
+		this.x = x;
+		this.y=y;
+		this.button=button;
+	}
+	
+
+}
